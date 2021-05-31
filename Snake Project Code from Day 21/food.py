a0758d9286048e8708640c19a2448cfae884729b
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2021-05-31 23:24:56 +0530

    adding snake game

--- Snake Project Code from Day 21/food.py
+++ Snake Project Code from Day 21/food.py
@@ -1,17 +1,19 @@
 from turtle import Turtle
-from random import randint
+import random
+
 
 class Food(Turtle):
+
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(0.5,0.5)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
-        self.goto(randint(-280,280),randint(-280,280))
+        self.refresh()
 
     def refresh(self):
-        self.goto(randint(-280,280),randint(-280,280))
-
-
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
